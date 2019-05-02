0973cc153fcbb74a956bf8cc7944647749de6004
Liwop38 <morin.tristan1@gmail.com>
2019-05-02 20:37:01 +0200

    day 4 b

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
     @like = Like.create(user_id:current_user.id,gossip_id:params[:gossip_id])
-    redirect_to gossip_path(params[:gossip_id])
+    redirect_back(fallback_location: root_path)
   end
   def destroy
     @like = Like.destroy(params[:id])
-    redirect_to gossip_path(params[:gossip_id])
+    redirect_back(fallback_location: root_path)
   end
 
 end
